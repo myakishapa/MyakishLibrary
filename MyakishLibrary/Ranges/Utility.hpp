58f996ab794536e7cc98bbd5f4f937348050f850
Myakish <hui1337228666@gmail.com>
2025-09-14 04:23:33 +0000

    ranges Borrow and FindElementOr

--- MyakishLibrary/Ranges/Utility.hpp
+++ MyakishLibrary/Ranges/Utility.hpp
@@ -32,4 +32,49 @@ namespace myakish::ranges
     };
     inline constexpr FindElementIfFunctor FindElementIf;
 
+
+    struct FindElementOrFunctor : functional::ExtensionMethod
+    {
+        template<std::ranges::input_range Range, class OrElse, class Predicate, class Projection = std::identity>
+        std::ranges::range_value_t<Range> operator()(Range&& range, OrElse&& orElse, const Predicate& predicate, Projection projection = {}) const
+        {
+            auto it = std::ranges::find_if(range, predicate, projection);
+            if (it != std::ranges::end(range)) return *it;
+            else return std::forward<OrElse>(orElse);
+        }
+    };
+    inline constexpr FindElementOrFunctor FindElementOr;
+
+
+    template<std::ranges::range Underlying>
+    struct ForceBorrowedView : std::ranges::view_interface<ForceBorrowedView<Underlying>>
+    {
+        Underlying&& range;
+
+        ForceBorrowedView(Underlying&& range) : range(std::forward<Underlying>(range)) {}
+
+        auto begin() const
+        {
+            return std::ranges::begin(range);
+        }
+        auto end() const
+        {
+            return std::ranges::end(range);
+        }
+    };
+    template<std::ranges::range Underlying>
+    ForceBorrowedView(Underlying&&) -> ForceBorrowedView<Underlying>;
+
+    struct BorrowFunctor : functional::ExtensionMethod
+    {
+        template<std::ranges::range Underlying>
+        auto operator()(Underlying&& range) const
+        {
+            return ForceBorrowedView(std::forward<Underlying>(range));
+        }
+    };
+    inline constexpr BorrowFunctor Borrow;
 }
+
+template<std::ranges::range Underlying>
+constexpr bool std::ranges::enable_borrowed_range<myakish::ranges::ForceBorrowedView<Underlying>> = true;
